8eac120d097206a1577c60858b339f5ef47856bc
Alex <423745+alexgenovese@users.noreply.github.com>
2024-09-01 14:58:41 +0200

    fixed

--- __init__.py
+++ __init__.py
@@ -6,7 +6,10 @@
 """
 
 from .nodes.HF_LLM_Serverless import HF_QuestionAnswer
+from .nodes.Joy_Caption import JoyPipeline, Joy_caption, Joy_caption_load
 
 NODE_CLASS_MAPPINGS = { 
-    "HF_QuestionAnswer": HF_QuestionAnswer
+    "HF_QuestionAnswer": HF_QuestionAnswer,
+    "Joy_caption":Joy_caption,
+    "Joy_caption_load":Joy_caption_load
 }
---
+++ nodes/utils.py
@@ -0,0 +1,8 @@
+import torch
+from PIL import Image
+import numpy as np
+import cv2 as cv
+
+
+def tensor2pil(t_image: torch.Tensor)  -> Image:
+    return Image.fromarray(np.clip(255.0 * t_image.cpu().numpy().squeeze(), 0, 255).astype(np.uint8))
